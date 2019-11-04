56d28ee894dcaf17a4b6ac3efa1996f5980727ec
Ubuntu <ubuntu@ip-172-31-24-146.ec2.internal>
2019-11-04 22:31:02 +0000

    send file to git hub

---
+++ break_program.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by : Mariam Hemdan
+# Created on : October 2019
+# This module uses a break statement
+
+import random
+
+
+def main():
+    # This function uses a break statement
+
+    # input
+    random_number = random.randint(1, 9+1)  # a number between 1 and 10
+    print("")
+
+    # process
+    while True:
+        number_guessing_game = int(input("Enter a number: "))
+        if number_guessing_game == random_number:
+                print("you guess it right")
+                break
+
+
+if __name__ == '__main__':
+    main()
